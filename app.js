9ccd3f14a56f2f5460e80a83672f92691b243be5
aditya-k-m <adityakrmishra0@gmail.com>
2020-04-11 20:45:31 +0530

    Improve parameters for custom Lists

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const app = express();
 const date = require(__dirname + "/date.js");
+const _ = require("lodash");
 //got the dependecies ready
 
 /*setting up for use*/
@@ -18,13 +19,13 @@ mongoose.connect("mongodb://localhost:27017/toDoListProject", {
 });
 
 //initializations
-var itemsArray = [];
-const itemSchema = {
+var itemsArray = []; //array is used to transfer data
+const itemSchema = { //Schema for single item
   name: String
 };
 const Item = mongoose.model("Item", itemSchema);
 //setting up the custom Lists collection
-const listSchema = {
+const listSchema = { //schema for a custom List
   name: String,
   value: [itemSchema]
 };
@@ -36,16 +37,11 @@ app.get("/", function(req, res) {
     if (err) {
       console.log("Error in reading from the DB.");
     } else {
-      // let i = 0;
-      // items.forEach((item, i) => {
-      //   itemsArray[i] = items[i].name;
-      //   i = i + 1;
-      // });
       itemsArray = items;
       console.log("Successfully retrived the items and transferred it to the array.");
     }
   });
-  console.log("Rendering page with array : "+itemsArray);
+  console.log("Rendering page with array : " + itemsArray);
   res.render("index", {
     param: "",
     dayName: date.getDayandDate(),
@@ -61,12 +57,12 @@ app.get("/about", function(req, res) {
 //setting up the post request to the home route
 app.post("/", function(req, res) {
   console.log(req.body);
-  if (req.body.ButtonPress == "removeAll"){
+  if (req.body.ButtonPress == "removeAll") {
     console.log("Removing All Data");
     itemsArray = [];
-    Item.deleteMany({},function(err){
-      if(err)
-      console.log("Error removing data");
+    Item.deleteMany({}, function(err) {
+      if (err)
+        console.log("Error removing data");
       else {
         console.log("All data removed");
       }
@@ -80,99 +76,103 @@ app.post("/", function(req, res) {
 });
 
 //post request for deleting single item
-app.post("/delete", function(req, res){
-  if (req.body.listName == ""){
-  itemsArray.splice(itemsArray.indexOf(req.body.CheckBox), 1);
-  Item.deleteOne({name: req.body.CheckBox}, function(err){
-    if(err){
-      console.log("error in deleting single item");
-    }
-    else {
-      console.log("Single Data removed");
-    }
-  });
-  res.redirect("/");
-}
- else{
-   List.findOneAndUpdate({name: req.body.listName}, {$pull: {value: {name: req.body.CheckBox}}}, function(err, result){
-     if(!err){
-       res.redirect("/"+req.body.listName);
-     }
-     else{
-       console.log("Error Deleting single item in custom list.");
-     }
-   });
- }
+app.post("/delete", function(req, res) {
+  if (req.body.listName == "") {
+    itemsArray.splice(itemsArray.indexOf(req.body.CheckBox), 1);
+    Item.deleteOne({
+      name: req.body.CheckBox
+    }, function(err) {
+      if (err) {
+        console.log("error in deleting single item");
+      } else {
+        console.log("Single Data removed");
+      }
+    });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate({
+      name: req.body.listName
+    }, {
+      $pull: {
+        value: {
+          name: req.body.CheckBox
+        }
+      }
+    }, function(err, result) {
+      if (!err) {
+        res.redirect("/" + req.body.listName);
+      } else {
+        console.log("Error Deleting single item in custom list.");
+      }
+    });
+  }
 });
 
 //setting up the routes for custom pages
-app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
-  List.findOne({name: customListName}, function(err, listFound){
-    if(!err){
-      if(!listFound){
+app.get("/:customListName", function(req, res) {
+  let customListName = req.params.customListName;
+  customListName = _.capitalize([string = customListName]);
+  List.findOne({
+    name: customListName
+  }, function(err, listFound) {
+    if (!err) {
+      if (!listFound) {
         //Create New List
         const list = new List({
           name: customListName,
-          value: []
+          value: [{
+            name: "Buffer Item"
+          }]
         });
         list.save();
-        res.redirect("/"+customListName);
-      }
-      else{
+        res.redirect("/" + customListName);
+      } else {
         res.render("index", {
-            param: customListName,
-            dayName: listFound.name,
-            listItems: listFound.value
+          param: customListName,
+          dayName: listFound.name,
+          listItems: listFound.value
         });
       }
-    }
-    else{
+    } else {
       console.log("Error retriving list data");
     }
   });
-  // List.find(function(err, lists){
-  //   if(err){
-  //     console.log("Error retriving the lists db.");
-  //   }
-  //   else{
-  //     itemsArray = lists;
-  //     console.log("Successfully retrived data from lists db.");
-  //   }
-  });
+});
 
 //setting up post requests for custom routes
-app.post("/:customListName", function(req, res){
+app.post("/:customListName", function(req, res) {
   const customListName = req.params.customListName;
   console.log(req.body);
-  if (req.body.ButtonPress == "removeAll"){
+  if (req.body.ButtonPress == "removeAll") {
     console.log("Removing All Data");
     itemsArray = [];
-    List.deleteOne({name: customListName},function(err){
-      if(err)
-      console.log("Error removing data");
+    List.deleteOne({
+      name: customListName
+    }, function(err) {
+      if (err)
+        console.log("Error removing data");
       else {
         console.log("All data removed");
       }
     });
-    res.redirect("/"+customListName);
+    res.redirect("/" + customListName);
+  } else {
+    const item = new Item({
+      name: req.body.inputItem
+    });
+    List.findOne({
+      name: customListName
+    }, function(err, foundList) {
+      if (err) {
+        console.log("error updating custom List");
+      } else {
+        foundList.value.push(item);
+        foundList.save();
+        console.log("Successfully updated custom List, now redirecting");
+        res.redirect("/" + customListName);
+      }
+    });
   }
-  else{
-  const item = new Item({
-    name: req.body.inputItem
-  });
-  List.findOne({name: customListName}, function(err, foundList){
-    if(err){
-      console.log("error updating custom List");
-    }
-    else {
-      foundList.value.push(item);
-      foundList.save();
-      console.log("Successfully updated custom List, now redirecting");
-      res.redirect("/"+customListName);
-    }
-  });
-}
 });
 
 //setting up the srever at port
